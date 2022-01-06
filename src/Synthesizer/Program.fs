8d6b4c0745419495d5bc194246652dde7a8ea6fa
Caton Clément <clement.caton@algosup.com>
2022-01-06 16:37:04 +0100

    added the other waves

--- src/Synthesizer/Program.fs
+++ src/Synthesizer/Program.fs
@@ -1,9 +1,15 @@
-﻿open System.IO
+open System.IO
 
-let squareWave x amplitude frequence= 
-    amplitude * sign(sin(((2. * 3.149 * x * frequence)))) 
+let sinWave x frequence amplitude =
+    amplitude * sin(float frequence * x * 3.14159)
 
-let triangleWave x  amplitude frequence= 
+let sawWave x frequence amplitude =
+    2. * amplitude * ( ( (x)/(float frequence) ) - floor( ( (1.) / (2.) ) + ( (x) / (float frequence) ) ) )
+
+let squareWave x frequence amplitude = 
+    amplitude * float(sign(sin(((2. * 3.149 * x * float frequence)))))
+
+let triangleWave x frequence amplitude = 
     ((amplitude)/(3.14)) * asin(sin((3.14*2.*frequence) * x))
 
 /// Write WAVE PCM soundfile (8KHz Mono 8-bit)
@@ -29,6 +35,19 @@ let write stream (data:byte[]) =
 
 let sample x = (x + 1.)/2. * 255. |> byte 
 
-let data = Array.init 16000 (fun i -> (2. * (((float i)/(40.))-floor(((1.)/(2.))+((float i)/(40.))))) + ((2.)/(3.14))* asin(3.*sin(3.14 * float i)) |> sample)
-let stream = File.Create("tonje.wav")
-write stream data
+let dataSin = Array.init 16000 (fun x -> sinWave x 200. 1. |> sample)
+let streamSin = File.Create("toneSin.wav")
+
+let dataSaw = Array.init 16000 (fun x -> sawWave x 200. 1. |> sample)
+let streamSaw = File.Create("toneSaw.wav")
+
+let dataSquare = Array.init 16000 (fun x -> squareWave x 200. 1. |> sample)
+let streamSquare = File.Create("toneSquare.wav")
+
+let dataTriangle = Array.init 16000 (fun x -> triangleWave x 200. 1. |> sample)
+let streamTriangle = File.Create("toneTriangle.wav")
+
+write streamSin dataSin
+write streamSaw dataSaw
+write streamSquare dataSquare
+write streamTriangle dataTriangle
