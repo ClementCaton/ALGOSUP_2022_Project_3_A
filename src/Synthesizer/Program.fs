0a5376ca93d44e8943f867f5369b0385ee38af52
Léo CHARTIER <leo.chartier@algosup.com>
2022-01-07 16:08:00 +0100

    [Not working] Merge attempt for 16 bit changes
    Needs fixing

--- src/Synthesizer/Program.fs
+++ src/Synthesizer/Program.fs
@@ -1,83 +1,70 @@
+﻿open System
 open System.IO
 
-let pi = 3.141592653589793
+let pi = Math.PI
+let freq = 440. // In Hertz
+let sampleRate = 44100 // In Hertz
+let amplitude = 0.25
+let duration = 5. // In seconds
+let pcmFormat = 1s
+let nbChannels = 1
+let bytesPerSample = 2
 
 let makeOverdrive x multiplicator=
     if x < (-1. * multiplicator) then (-1. * multiplicator) else
     if x > 1. * multiplicator then 1. * multiplicator else
     x
 
-let sinWave x frequence amplitude =
-    amplitude * sin(2. * pi * x * frequence)
+let sinWave t frequence amplitude =
+    amplitude * sin (2. * pi * t * frequence)
 
-let sawWave x frequence amplitude =
-    2. * amplitude * ( x * frequence - floor(0.5 +  x * frequence))
+let sawWave t frequence amplitude =
+    2. * amplitude * (t * frequence - floor (0.5 +  t * frequence))
 
-let squareWave x frequence amplitude= 
-    amplitude * float(sign(sin(2. * pi * x * frequence)))
+let squareWave t frequence amplitude =
+    amplitude * float (sign (sin (2. * pi * t * frequence)))
 
-let triangleWave x frequence amplitude= 
-    2. * amplitude * asin(sin(2. * pi * x * frequence)) / pi
+let triangleWave t frequence amplitude =
+    2. * amplitude * asin (sin (2. * pi * t * frequence)) / pi
 
-/// Write WAVE PCM soundfile (8KHz Mono 8-bit)
-let write stream (data:byte[]) =
+
+let bitsPerSample = bytesPerSample * 8
+
+/// Write WAVE PCM soundfile
+let write stream (data: byte []) =
+    let byteRate = sampleRate * nbChannels * bytesPerSample
+    let blockAlign = uint16 (nbChannels * bytesPerSample)
+
     use writer = new BinaryWriter(stream)
     // RIFF
     writer.Write("RIFF"B)
-    let size = 36 + data.Length in writer.Write(size)
+    writer.Write(36 + data.Length) // File size
     writer.Write("WAVE"B)
     // fmt
     writer.Write("fmt "B)
-    let headerSize = 16 in writer.Write(headerSize)
-    let pcmFormat = 1s in writer.Write(pcmFormat)
-    let mono = 1s in writer.Write(mono)
-    let sampleRate = 44100 in writer.Write(sampleRate)
-    let byteRate = sampleRate in writer.Write(byteRate)
-    let blockAlign = 1s in writer.Write(blockAlign)
-    let bitsPerSample = 8s in writer.Write(bitsPerSample)
+    writer.Write(16) // Header size
+    writer.Write(pcmFormat)
+    writer.Write(uint16 nbChannels)
+    writer.Write(sampleRate)
+    writer.Write(byteRate)
+    writer.Write(blockAlign)
+    writer.Write(uint16 bitsPerSample)
     // data
     writer.Write("data"B)
     writer.Write(data.Length)
     writer.Write(data)
 
-let sample x = (x + 1.)/2. * 255. |> byte 
+let generate func =
+    let size = int (duration * float sampleRate)
+    let toBytes x =
+        let unitary = (x + 2. ** (float bytesPerSample - 1.)) / 2.
+        let upscaled = round (unitary * (256. ** (float bytesPerSample))) - (if unitary = 1. then 1. else 0.)
+        [ for k in 0..(bytesPerSample-1) do byte (upscaled/(256.**k)) ]
 
-let stream = File.Create("tone.wav")
-let data = Array.init 44100 (fun i -> 
-    (sinWave (float i/44100.) 4. 0.25) + 
-    (sawWave (float i / 44100.) 4. 0.25) +
-    (squareWave (float i/44100.) 4. 0.25) + 
-    (triangleWave (float i / 44100.) 4. 0.25)
-    |> sample)
+    let getData = float >> (fun x -> (x / float sampleRate)) >> func freq amplitude >> toBytes
+    [ for i in 0 .. (size - 1) do yield! getData i ] |> Array.ofList
 
-let streamSin = File.Create("toneSin.wav")
-let dataSin = Array.init 44100 (fun i -> 
-    makeOverdrive(
-        sinWave (float i/44100.)1. 1.) 0.8 
-        |> sample)
-
-
-let streamSaw = File.Create("toneSaw.wav")
-let dataSaw = Array.init 44100 (fun i -> 
-    makeOverdrive(
-        sawWave (float i/44100.)1. 1.) 0.8 
-        |> sample)
-
-
-let streamSquare = File.Create("toneSquare.wav")
-let dataSquare = Array.init 44100 (fun i -> 
-    makeOverdrive(
-        squareWave (float i/44100.)1. 1.) 0.8 
-        |> sample)
-
-let streamTriangle = File.Create("toneTriangle.wav")
-let dataTriangle = Array.init 44100 (fun i -> 
-    makeOverdrive(
-        triangleWave (float i/44100.)1. 1.) 0.8 
-        |> sample)
-
-write stream data
-write streamSin dataSin
-write streamSaw dataSaw
-write streamSquare dataSquare
-write streamTriangle dataTriangle
+write (File.Create("toneSin.wav")) (generate sinWave)
+write (File.Create("toneSquare.wav")) (generate squareWave)
+write (File.Create("toneTriangle.wav")) (generate triangleWave)
+write (File.Create("toneSaw.wav")) (generate sawWave)
