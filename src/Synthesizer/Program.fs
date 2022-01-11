a4e391e35ca4ad4e57a305059c1f57a322f96a79
TheoDct <91249762+TheoDct@users.noreply.github.com>
2022-01-11 10:44:33 +0100

    remove invisible characters

--- src/Synthesizer/Program.fs
+++ src/Synthesizer/Program.fs
@@ -1,10 +1,10 @@
-namespace Synthesizer
+﻿namespace Synthesizer
 
 module Program =
     let t = API.getNoteOffset 4 "C" 330
     printfn $"test= {t}" 
     /////////////////////////////////////////////////////////////////////////////////
-﻿open System
+open System
 open System.IO
 
 let freq = 440. // In Hertz
