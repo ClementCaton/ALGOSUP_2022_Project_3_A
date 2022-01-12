6d30a5ea9c03ae1bb8587b38bc8daf8896e8ddaa
Léo CHARTIER <leo.chartier@algosup.com>
2022-01-12 15:15:03 +0100

    Added unions to clear code and added durations

--- src/Synthesizer/API.fs
+++ src/Synthesizer/API.fs
@@ -3,7 +3,7 @@ namespace Synthesizer
 open System
 open System.IO
 
-module API = 
+module API =
     
     let getNoteFreq octav note =
         CalcNoteFreq(octav, note).Output
@@ -12,20 +12,14 @@ module API =
         CalcNoteFreq(octav, note, aFourFreq).Output
 
 
-    let createSound freq duration waveType=
-        let creator = createSoundData(frequency0 = freq, duration0 = duration)
-        match waveType with
-        | "sin" -> creator.sinWave
-        | "square" -> creator.squareWave
-        | "triangular" -> creator.triangleWave
-        | "saw" -> creator.sawWave
-        | _ -> creator.sinWave
+    let createSound freq duration waveType =
+        createSoundData(frequency0 = freq, duration0 = duration).create(waveType)
 
     let writeToWav path data =
         writeWav().Write (File.Create(path)) (data)
 
 
-    let note octav note = 
-        let freq = getNoteFreq octav note
-        let soundData = createSound 440. 3. ""
+    let note duration note octave =
+        let freq = getNoteFreq note octave
+        let soundData = createSound freq duration Sin
         writeToWav "wave.wav" soundData
