0409a965cec1f03fd44cc370c0e0f51a3c348770
TheoDct <91249762+TheoDct@users.noreply.github.com>
2022-01-21 00:23:29 +0100

    documentation for the frequency analizer

--- src/Synthesizer/frequencyAnalyzer.fs
+++ src/Synthesizer/frequencyAnalyzer.fs
@@ -2,8 +2,26 @@ namespace Synthesizer
 
 open System
 open System.Numerics
+open MathNet.Numerics //can be remplaced by using the already created waveforms
+open MathNet.Numerics.IntegralTransforms
+
+// Used for complex numbers
+// open System.Numerics
+// used to display charts 
+// open System.Windows.Forms.DataVisualization.Charting 
 
 module frequencyAnalysis =
+
+// Waveform can be described by adding sinusoidal with some frequencies magnitudes and phase angles 
+
+// We need to put 2 boxes to display the waveform and another to display the frequency after the fast fourier
+// The objectives is to display the sinusoides compiled in a single columns 
+// a wave with a magnitude of 10 in 60 hertz mean a sinusoid at 60 hertz to a row 10 to describes the form
+
+// We need to be able to compile more waves to mix with the first and display other sinusoid
+// The magnitude and the phases of these waves should be changeable 
+
+
 
     let rec fft = function
     | []  -> []
@@ -19,3 +37,10 @@ module frequencyAnalysis =
         |> List.unzip
         ||> List.append
 
+let input = [for x in 0. .. 15. -> cos(x)  + cos(4.0 * x)]
+    
+let output = 
+    input   
+    |> List.map (fun r -> Complex(r, 0.)) 
+    |> fft
+    |> List.map (fun c -> c.Real)
