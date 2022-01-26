5d27d5ac9bb7c45556f88e9a62321d88f81e52a5
Mathieu Chaput <mathieu.chaput@algosup.com>
2022-01-26 16:16:27 +0100

    adding tests on filters

--- src/Synthesizer.Test/UITest/FiltersTest.fs
+++ src/Synthesizer.Test/UITest/FiltersTest.fs
@@ -18,15 +18,59 @@ let changeAmplitudeTest() =
     Assert.That(mockData,Is.EqualTo(data))
     Assert.That(data,Is.InstanceOf<List<float>>())
 
-// [<Test>]
+[<Test>]
 let addTwoWavesTest() =
     let creater0 = new createSoundData (amplitude0 = 0.)
     let creater1 = new createSoundData (amplitude0 = 1.)
     let data = addTwoWaves 0.5 (creater1.create Sin) (creater0.create Sin)
    
-    let creater2 = new createSoundData (amplitude0 = 1)
+    let creater2 = new createSoundData (amplitude0 = 0.5)
     let mockData = (creater2.create Sin)
 
-    Assert.That(mockData.[200],Is.EqualTo(data.[200]))
+    Assert.That(mockData,Is.EqualTo(data))
     Assert.That(data,Is.InstanceOf<List<float>>())
 
+[<Test>]
+let addTwoWavesWithASilentTest() =
+    let creater0 = new createSoundData (amplitude0 = 0.)
+    let creater1 = new createSoundData (amplitude0 = 1.)
+    let data = addTwoWaves 0.5 (creater1.create Sin) (creater0.create Sin)
+   
+    let creater2 = new createSoundData (amplitude0 = 0.5)
+    let mockData = (creater2.create Sin)
+
+    Assert.That(mockData,Is.EqualTo(data))
+    Assert.That(data,Is.InstanceOf<List<float>>())
+
+[<Test>]
+let addTwoWavesFirstRatioTest() =
+    let creater0 = new createSoundData (amplitude0 = 0.5)
+    let creater1 = new createSoundData (amplitude0 = 1.)
+    let data = addTwoWaves 1. (creater1.create Sin) (creater0.create Sin)
+   
+    let creater2 = new createSoundData (amplitude0 = 0.5)
+    let mockData = (creater2.create Sin)
+
+    Assert.That((creater0.create Sin),Is.EqualTo(data))
+    Assert.That(data,Is.InstanceOf<List<float>>())
+
+[<Test>]
+let addTwoWavesSecondRatioTest() =
+    let creater0 = new createSoundData (amplitude0 = 0.5)
+    let creater1 = new createSoundData (amplitude0 = 1.)
+    let data = addTwoWaves 0. (creater1.create Sin) (creater0.create Sin)
+
+    Assert.That((creater1.create Sin),Is.EqualTo(data))
+    Assert.That(data,Is.InstanceOf<List<float>>())
+    
+[<Test>]
+let addModulationWithASilentTest() =
+    let creater0 = new createSoundData (amplitude0 = 1.)
+    let creater1 = new createSoundData (amplitude0 = 0.)
+    let data = addModulation 0.5 (creater1.create Sin) (creater0.create Sin)
+   
+    let creater2 = new createSoundData (amplitude0 = 1.)
+    let mockData = (creater2.create Sin)
+
+    Assert.That(mockData,Is.EqualTo(data))
+    Assert.That(data,Is.InstanceOf<List<float>>())
