671c485336cb00cccd29e0b98de21f86c4584fbb
Léo CHARTIER <leo.chartier@algosup.com>
2022-01-26 11:48:37 +0100

    Cleanup and improve high/low pass

--- src/Synthesizer/UI/Filters.fs
+++ src/Synthesizer/UI/Filters.fs
@@ -3,20 +3,6 @@ namespace Synthesizer
 module Filter =
     open System
 
-    //let addTwoWaves ratio (x:List<float>) (y:List<float>) = 
-    //    let mutable output = List.empty
-    //    if not (x.Length = y.Length) then
-    //        let diff = Math.Abs(x.Length - y.Length)
-    //        let endArray = [for i in [0 .. diff] do 0.0]
-    //        if x.Length > y.Length then
-    //            let newY = List.append y endArray
-    //            output <- List.init x.Length (fun i -> (x[i] * ratio) + (newY[i] * (1.-ratio)))
-    //        else 
-    //            let newX = List.append x endArray
-    //            output <- List.init y.Length (fun i -> (newX[i] * ratio) + (y[i] * (1.-ratio)))
-    //    else 
-    //        output <- List.init x.Length (fun i -> (x[i] * ratio) + (y[i] * (1.-ratio)))
-    //    output
     let changeAmplitude multiplicator (x:List<float>) =
         x |> List.map (( * ) multiplicator)
 
@@ -40,36 +26,11 @@ module Filter =
         output <- changeAmplitude oldMax output
         Utility.makeOverdrive 1. output
 
-
     let makeOverdrive multiplicator (x:List<float>) =
         [for i in x do 
             if i < (-1. * multiplicator) then (-1. * multiplicator) else
             if i > (1. * multiplicator) then (1. * multiplicator) else
             i]
-
-    // let createEcho (startIndex:int) (endIndex:int) (delay:float) (nbEcho:int) (x:List<float>) = //takes the whole sound and echoes it
-    //     let silenceDelay = [for i in 0. .. delay do 0.]
-    //     //let silenceEcho = [for i in 0 .. ( endIndex - startIndex ) do 0.]
-    //     let echoSample = x[startIndex..endIndex]
-
-    //     let mutable (output:List<List<float>>) = List.empty
-    //     let mutable buffer = List.empty
-
-    //     for i in [0 .. nbEcho] do
-    //         buffer <- List.empty
-    //         for a in [0 .. i] do
-    //             buffer <- buffer |> List.append silenceDelay
-    //             //buffer <- buffer |> List.append silenceEcho
-    //         buffer <- List.append buffer echoSample
-    //         output <- output @ [buffer]
-
-    //     let mutable returnValue = output[0]
-    //     for i in [(output.Length - 1).. -1 ..1] do 
-    //         returnValue <- addTwoWaves returnValue output[i] 0.66
-    //     let silence = [for i in 0 .. (startIndex - 1) do 0.]
-    //     returnValue <- List.append silence returnValue
-    //     addTwoWaves x returnValue
-
 
     let cutCorners limit (data:List<float>) =
         let step = 1. / float limit
@@ -77,25 +38,6 @@ module Filter =
         let endVals = List.map2(fun x i -> x * step * i) data[data.Length-limit..] [float limit .. -1. .. 1.]
 
         List.append (List.append startVals data[limit .. data.Length-limit-1]) endVals
-
-    let createDelay (start:float) (ending:float) (delay:float) sampleRate (data:List<float>) =
-        let (newData) = [
-            for i in (int (start*float sampleRate)) .. (int(ending*float sampleRate)) do 
-                if i < data.Length then
-                    yield data.[i]
-        ]
-        // printfn "%A %A %A" (int (delay * float sampleRate)) delay sampleRate
-        let mutable inc = 0
-        let fData = [
-            for i in 0 .. data.Length-1 do
-                if i > (int (start * float sampleRate)+(int (delay * float sampleRate))) && i < (int (ending*float sampleRate)+(int (delay * float sampleRate))) then
-                    // printfn "%A %A %A %A" newData.Length inc  i data.Length
-                    yield (newData.[inc] + data.[i])/2.
-                    inc <- inc + 1
-                else
-                    yield data.[i]
-        ]
-        fData
 
     let reverb (nbEcho:int) (decay:float) (delay:float) (sampleRate:float) (dryData:List<float>) = 
         let rec revebInner (nbEcho:int) (decay:float) (delay:float) (sampleRate:float) (wetData:List<float>) (dryData:List<float>) =   // This is also echo
@@ -134,22 +76,22 @@ module Filter =
             rep <- rep - 1
         actualData
 
-    // enveloppe stuff
-    let pinchAmp (dataPoints0: List<float * float>) (sampleRate:float) (data:List<float>) =
-        let dataPoints = if (fst dataPoints0[0] <> 0.) then (0., 0.) :: dataPoints0 else dataPoints0
-
-        let calcSegment (fromTime:float) (toTime:float) fromAmp toAmp =
-            let step = (toAmp - fromAmp) / (toTime - fromTime)
-            List.mapi(fun i flatPoint -> (flatPoint * (fromAmp + (float step * float i)))) data[int fromTime .. int toTime]
-
-        let output = List.map2(fun fromT toT -> calcSegment (sampleRate * (fst fromT)) (sampleRate * (fst toT)) (snd fromT) (snd toT)) dataPoints[ .. dataPoints.Length-2] dataPoints[1 ..]
-
-        output |> List.concat
-
-    let enveloppe (sampleRate:float) sustain attack hold0 decay0 release0 (data:List<float>) = //release substracts from hold because I don't have the data for the release periode
+        
+    let envelope sustain attack hold0 decay0 release0 (sampleRate:float) (data:List<float>) = //release substracts from hold because I don't have the data for the release periode
         let hold = hold0 + attack
         let decay = hold + decay0
         let release = (float data.Length/float sampleRate) - release0
+        
+        let pinchAmp (dataPoints0: List<float * float>) (sampleRate:float) (data:List<float>) =
+            let dataPoints = if (fst dataPoints0[0] <> 0.) then (0., 0.) :: dataPoints0 else dataPoints0
+    
+            let calcSegment (fromTime:float) (toTime:float) fromAmp toAmp =
+                let step = (toAmp - fromAmp) / (toTime - fromTime)
+                List.mapi(fun i flatPoint -> (flatPoint * (fromAmp + (float step * float i)))) data[int fromTime .. int toTime]
+    
+            let output = List.map2(fun fromT toT -> calcSegment (sampleRate * (fst fromT)) (sampleRate * (fst toT)) (snd fromT) (snd toT)) dataPoints[ .. dataPoints.Length-2] dataPoints[1 ..]
+    
+            output |> List.concat
 
         pinchAmp ([(0., 0.); (attack, 1.); (hold, 1.); (decay, sustain); (release, sustain); ((float data.Length/float sampleRate), 0.)]) sampleRate data //error here
         //pinchAmp data ([(0., 0.); (attack, 1.); (hold, 1.); (decay, sustain); ((float data.Length/float sampleRate), sustain); (release, 0.)]) sampleRate  //error here
@@ -212,27 +154,27 @@ module Filter =
         let alpha = dt / (RC + dt)
         let alpha2 = 1. - alpha
 
-        // TODO: Refactorize and make faster
-        let mutable y = [alpha * data.[0]]
-        let mutable y' = [alpha * data.[0]]
-        for x in List.tail data do
-            y' <- y' @ [ alpha * x + alpha2 * (List.last y') ]
-            if (List.length y') = 10000 then
-                y <- y @ y'[1..]
-                y' <- [List.last y']
-        y @ y'[1..]
+        let mutable last = alpha * data.[0]
+        [last] @ (
+            data
+            |> List.tail
+            |> List.map (fun x ->
+                last <- alpha * x + alpha2 * last
+                last
+            )
+        )
 
     let highPass sampleRate cutoffFreq (data:List<float>) =
         let RC = 1. / (2. * Math.PI * cutoffFreq)
         let dt = 1. / sampleRate
         let alpha = dt / (RC + dt)
 
-        // TODO: Refactorize and make faster
-        let mutable y = [data.[0]]
-        let mutable y' = [data.[0]]
-        for i in 1..(List.length data - 1) do
-            y' <- y' @ [ alpha * (List.last y' + data.[i] - data.[i-1]) ]
-            if (List.length y') = 10000 then
-                y <- y @ y'[1..]
-                y' <- [List.last y']
-        y @ y'[1..]
+        let mutable last = data.[0]
+        [last] @ (
+            (data.[1..(List.length data - 1)], data.[0..(List.length data - 2)])
+            ||> List.map2 ( - )
+            |> List.map (fun x ->
+                last <- alpha * (last + x)
+                last
+            )
+        )
