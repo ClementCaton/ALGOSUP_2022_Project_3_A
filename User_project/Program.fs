5fd7320d1615409ad2ed08cf53d5ac0c2d669c51
TheoDct <91249762+TheoDct@users.noreply.github.com>
2022-01-27 22:33:54 +0100

    update "API" to "Synth"

--- User_project/Program.fs
+++ User_project/Program.fs
@@ -11,60 +11,60 @@ open Synthesizer
 
 module Program =
 
-    let basicSound = API.note (Seconds 0.5) Note.A 4
+    let basicSound = Synth.note (Seconds 0.5) Note.A 4
     let reverb = Filter.reverb 50 0.8 0.2 44100. basicSound
     let echo = Filter.reverb 5 0.7 1. 44100. basicSound
-    API.writeToWav "basic.wav" [basicSound]
-    API.writeToWav "reverb.wav" [reverb]
-    API.writeToWav "echo.wav" [echo]
-    API.writeToWav "basic.wav" [basicSound]
+    Synth.writeToWav "basic.wav" [basicSound]
+    Synth.writeToWav "reverb.wav" [reverb]
+    Synth.writeToWav "echo.wav" [echo]
+    Synth.writeToWav "basic.wav" [basicSound]
 
     // Custom duration
     let DottedEighth = Custom (1./8. * 1.5)
     let EighthAndHalf = Custom (1./8. + 1./2.)
 
     // Create the melodies
-    let mainMelody = API.compose [
-        API.note Eighth Note.D 4
-        API.note Eighth Note.E 4
-        API.note Eighth Note.F 4
-        API.note Eighth Note.F 4
-        API.note Eighth Note.G 4
-        API.note DottedEighth Note.E 4
-        API.note Sixteenth Note.D 4
-        API.note EighthAndHalf Note.C 4
+    let mainMelody = Synth.compose [
+        Synth.note Eighth Note.D 4
+        Synth.note Eighth Note.E 4
+        Synth.note Eighth Note.F 4
+        Synth.note Eighth Note.F 4
+        Synth.note Eighth Note.G 4
+        Synth.note DottedEighth Note.E 4
+        Synth.note Sixteenth Note.D 4
+        Synth.note EighthAndHalf Note.C 4
     ]
 
-    let secondMelody = API.compose [
-        API.note Half Note.Bb 3
-        API.silence Eighth
-        API.note DottedEighth Note.C 4
+    let secondMelody = Synth.compose [
+        Synth.note Half Note.Bb 3
+        Synth.silence Eighth
+        Synth.note DottedEighth Note.C 4
     ]
 
-    let secondHandHigh = API.compose [
-        API.note EighthAndHalf Note.Bb 2
-        API.note Half Note.C 3
+    let secondHandHigh = Synth.compose [
+        Synth.note EighthAndHalf Note.Bb 2
+        Synth.note Half Note.C 3
     ]
 
-    let secondHandLow = API.compose [
-        API.note EighthAndHalf Note.Bb 1
-        API.note Half Note.C 2
+    let secondHandLow = Synth.compose [
+        Synth.note EighthAndHalf Note.Bb 1
+        Synth.note Half Note.C 2
     ]
 
     // Superpose the melodies and write to file
-    let music = API.add [mainMelody; secondMelody; secondHandHigh; secondHandLow]
+    let music = Synth.add [mainMelody; secondMelody; secondHandHigh; secondHandLow]
 
 
-    API.writeToWav "wave.wav" [music]
+    Synth.writeToWav "wave.wav" [music]
 
     //frequence amplitude verticalShift phaseShift t
-    let input = API.add [API.note Whole Note.A 3;API.note Whole Note.A 4;API.note Whole Note.A 5]
-    API.writeToWav "A345.wav" [input]
+    let input = Synth.add [Synth.note Whole Note.A 3;Synth.note Whole Note.A 4;Synth.note Whole Note.A 5]
+    Synth.writeToWav "A345.wav" [input]
     let output = frequencyAnalysis.fourier input
-    API.preview "A 3,4,5 Analysis" output |> ignore
+    Synth.preview "A 3,4,5 Analysis" output |> ignore
 
     let mono = [music]
-    API.writeToWav "rickroll.wav" mono
+    Synth.writeToWav "rickroll.wav" mono
 
     let stereo = [music; mainMelody]
-    API.writeToWav "rickrollStereo.wav" stereo
+    Synth.writeToWav "rickrollStereo.wav" stereo
