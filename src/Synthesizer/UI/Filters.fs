6d5569000c424f646bb61be7522ad69a998763bb
Polonit <71769482+Louis-de-Lavenne-de-Choulot@users.noreply.github.com>
2022-01-31 12:16:21 +0100

    pinchAmp into Custom Envelope

--- src/Synthesizer/UI/Filters.fs
+++ src/Synthesizer/UI/Filters.fs
@@ -64,13 +64,7 @@ module Filter =
 
         Utility.add [dryData; (silence @ wetData)]
 
-        
-    let envelope sustain attack hold0 decay0 release0 (sampleRate:float) (data:List<float>) = //release substracts from hold because I don't have the data for the release periode
-        let hold = hold0 + attack
-        let decay = hold + decay0
-        let release = (float data.Length/float sampleRate) - release0
-        
-        let pinchAmp (dataPoints0: List<float * float>) (sampleRate:float) (data:List<float>) =
+    let CustomEnvelope (dataPoints0: List<float * float>) (sampleRate:float) (data:List<float>) =
             let dataPoints = if (fst dataPoints0[0] <> 0.) then (0., 0.) :: dataPoints0 else dataPoints0
     
             let calcSegment (fromTime:float) (toTime:float) fromAmp toAmp =
@@ -80,8 +74,15 @@ module Filter =
             let output = List.map2(fun fromT toT -> calcSegment (sampleRate * (fst fromT)) (sampleRate * (fst toT)) (snd fromT) (snd toT)) dataPoints[ .. dataPoints.Length-2] dataPoints[1 ..]
     
             output |> List.concat
+    
+    let envelope sustain attack hold0 decay0 release0 (sampleRate:float) (data:List<float>) = //release substracts from hold because I don't have the data for the release periode
+        let hold = hold0 + attack
+        let decay = hold + decay0
+        let release = (float data.Length/float sampleRate) - release0
+        
+        
 
-        pinchAmp ([(0., 0.); (attack, 1.); (hold, 1.); (decay, sustain); (release, sustain); ((float data.Length/float sampleRate), 0.)]) sampleRate data //error here
+        CustomEnvelope ([(0., 0.); (attack, 1.); (hold, 1.); (decay, sustain); (release, sustain); ((float data.Length/float sampleRate), 0.)]) sampleRate data //error here
         //pinchAmp data ([(0., 0.); (attack, 1.); (hold, 1.); (decay, sustain); ((float data.Length/float sampleRate), sustain); (release, 0.)]) sampleRate  //error here
 
 
