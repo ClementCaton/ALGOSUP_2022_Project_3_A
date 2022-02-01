fec64eb66e1e511f02df7c2cbd812b9b46929458
Mathieu Chaput <mathieu.chaput@algosup.com>
2022-02-01 14:22:09 +0100

    remove test about AddModulation and AddModulation

--- src/Synthesizer.Test/UITest/FiltersTest.fs
+++ src/Synthesizer.Test/UITest/FiltersTest.fs
@@ -49,37 +49,3 @@ let AddTwoWavesSecondRatioTest() =
 
     Assert.That(mockData,Is.EqualTo(data))
     Assert.That(data,Is.InstanceOf<List<float>>())
-    
-[<Test>]
-let AddModulationWithASilentTest() =
-    let data = AddModulation 0.5 (SoundData(amplitude0 = 0.).Create Silence) (SoundData(amplitude0 = 1.).Create Sin)
-    let mockData = (SoundData(amplitude0 = 1.).Create Sin)
-    
-    let roundData = data |> List.map (fun x -> Math.Round(x, 10))
-    let roundMockData = mockData |> List.map (fun x -> Math.Round(x, 10))
-
-    Assert.That(roundMockData,Is.EqualTo(roundData))
-    Assert.That(data,Is.InstanceOf<List<float>>())
-
-
-[<Test>]
-let AddModulationFirstRatioTest() =
-    let data = AddModulation 1. (SoundData(amplitude0 = 0.5).Create Sin) (SoundData(amplitude0 = 1.).Create Sin)
-    let mockData = SoundData(amplitude0 = 0.5).Create Sin
-
-    let roundData = data |> List.map (fun x -> Math.Round(x, 10))
-    let roundMockData = mockData |> List.map (fun x -> Math.Round(x, 10))
-
-    Assert.That(roundMockData.[300],Is.EqualTo(roundData.[300]))
-    Assert.That(roundData,Is.InstanceOf<List<float>>())
-
-[<Test>]
-let AddModulationSecondRatioTest() =
-    let data = AddModulation 0. (SoundData(amplitude0 = 0.).Create Sin) (SoundData(amplitude0 = 0.5).Create Sin)
-    let mockData = SoundData(amplitude0 = 0.5).Create Sin
-
-    let roundData = data |> List.map (fun x -> Math.Round(x, 10))
-    let roundMockData = mockData |> List.map (fun x -> Math.Round(x, 10))
-
-    Assert.That(roundMockData.[300],Is.EqualTo(roundData.[300]))
-    Assert.That(data,Is.InstanceOf<List<float>>())
