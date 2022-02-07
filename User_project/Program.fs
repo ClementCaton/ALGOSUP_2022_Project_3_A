e1bf78a1ec9e67c24df64dbb3c2ba6b1cd2b43e3
Polonit <71769482+Louis-de-Lavenne-de-Choulot@users.noreply.github.com>
2022-02-07 10:20:14 +0100

    play mac

--- User_project/Program.fs
+++ User_project/Program.fs
@@ -10,82 +10,20 @@ open Synthesizer
 
 
 module Program =
-    let filters = [
-        Filter.lowPass 44100 500;
-        Filter.highPass 44100 500;
-        Filter.flanger 0.1 0.1 44100;
-    ]
-
-    let input = Synth.add [Synth.note Whole Note.A 2; Synth.note Whole Note.A 3; Synth.note Whole Note.A 4; Synth.note Whole Note.A 5]
-
-    let a = Synth.ApplyFilters filters input
-
-    (*
-    let input = synth.Add [synth.Note Whole Note.A 2; synth.Note Whole Note.A 3; synth.Note Whole Note.A 4; synth.Note Whole Note.A 5]
-    printfn "Wanted:   %A" [CalcNoteFreq(Note.A, 2).Output; CalcNoteFreq(Note.A, 3).Output; CalcNoteFreq(Note.A, 4).Output; CalcNoteFreq(Note.A, 5).Output]
-    synth.WriteToWav "A345.wav" [input]
-    let output = synth.Fourier 44100. input
-    let freq = FrequencyAnalysis.LocalMaxValuesIndices 0.25 output
-    let amplitudes = output |> Map.filter (fun f _ -> List.contains f freq)
-    //printfn "%f %f %f %f" (List.average input) (List.average output) (List.sum input) (List.sum output)
-    printfn "Obtained: %A" freq
-    printfn "Amplitudes: %A" amplitudes
-    synth.PreviewMap "A 3,4,5 Analysis" output |> ignore
-    *)
-
-
-    // Among Us Drip
-    // https://musescore.com/user/5032516/scores/6519100
-    let DottedQuarter = Custom (1./4. * 1.5)
-    let TripletEighth = Custom (1./8. * 2./3.)
-    synth.bpm <- 94.
-
-    synth.waveType <- Triangular
-    let melody = synth.Compose [
-        synth.Silence Quarter
+    let synth = new Synth()
+    
+    let music = synth.Compose [
         synth.Note Eighth Note.C 5
         synth.Note Eighth Note.Eb 5
         synth.Note Eighth Note.F 5
         synth.Note Eighth Note.Gb 5
         synth.Note Eighth Note.F 5
         synth.Note Eighth Note.Eb 5
-        synth.Note DottedQuarter Note.C 5
+        synth.Note Eighth Note.C 5
         synth.Note Sixteenth Note.Bb 4
         synth.Note Sixteenth Note.D 5
         synth.Note Quarter Note.C 5
-        synth.Silence Half
-        synth.Note Eighth Note.C 5
-        synth.Note Eighth Note.Eb 5
-        synth.Note Eighth Note.F 5
-        synth.Note Eighth Note.Gb 5
-        synth.Note Eighth Note.F 5
-        synth.Note Eighth Note.Eb 5
-        synth.Note Half Note.Gb 5
-        synth.Note TripletEighth Note.Gb 5
-        synth.Note TripletEighth Note.F 5
-        synth.Note TripletEighth Note.Eb 5
-        synth.Note TripletEighth Note.Gb 5
-        synth.Note TripletEighth Note.F 5
-        synth.Note TripletEighth Note.Eb 5
     ]
-
-    synth.waveType <- Sin
-    let rhythmic = synth.Compose [
-        synth.Note Quarter Note.C 3
-        synth.Silence (Custom (13./8.))
-        synth.Note Eighth Note.G 2
-        synth.Note Quarter Note.C 3
-        synth.Silence (Custom (3./2.))
-        synth.Note Quarter Note.C 4
-    ]
-
-    let music =
-        [
-            (melody, 0.2)
-            (rhythmic, 0.8)
-        ]
-        |> Utility.AddFactor
-        |> List.replicate 8
-        |> List.concat
 
     synth.WriteToWav "amogus.wav" [music]
+    synth.PlayWav (float32 3) [music] |> ignore 
