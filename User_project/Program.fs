e0f9cbca684cb59668fb23b6de49ad0aabb1f509
harkaled2 <80251702+ivan-molnar@users.noreply.github.com>
2022-02-09 09:12:03 +0100

    corrected nuget package

--- User_project/Program.fs
+++ User_project/Program.fs
@@ -25,9 +25,9 @@ module Program =
     *)
 
     let basic = synth.Note (Seconds 2.) Note.A 4
-    let modWave = synth.Sound 100. (Seconds 2.) Sin
-    let fm = Filter.LFO_FM modWave 2. basic
+    // let modWave = synth.Sound 100. (Seconds 2.) Sin
+    // let fm = Filter.LFO_FM modWave 2. basic
     synth.WriteToWav "basic.wav" [basic]
-    synth.WriteToWav "modWave.wav" [modWave]
-    synth.WriteToWav "fm.wav" [fm]
+    // synth.WriteToWav "modWave.wav" [modWave]
+    // synth.WriteToWav "fm.wav" [fm]
 
